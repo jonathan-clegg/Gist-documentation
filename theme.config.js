5024dc50621051df06f122008afcfbaa4b9c182c
jonathan-clegg <109887699+jonathan-clegg@users.noreply.github.com>
2022-07-25 12:42:49 +1000

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – B Design System',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">B Design System</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Next.js/Figma Static Site Generator
       </span>
     </>
   ),
